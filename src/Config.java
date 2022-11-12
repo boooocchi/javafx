4286b75453479d4c4b580cc1ba9ecd700373d6ff
boooocchi@gmail.com <boooocchi@gmail.com>
2022-11-11 18:56:24 -0800

    demo

---
+++ src/Config.java
@@ -0,0 +1,7 @@
+public class Config {
+
+    protected static final String URL = "db.ejihhhcepgpnyxfizbkg.supabase.co:5432/postgres";
+    protected static final String USERNAME = "postgres";
+    protected static final String PASSWORD = "qO9ZGiHUhUnFLapB";
+    
+}
